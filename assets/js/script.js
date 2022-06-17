1add226634005500bc7351ea9eead3382e028c11
Anna Gabain <anna.gabain@outlook.com>
2022-06-17 20:24:53 +0000

    Create the addCost function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,20 +17,27 @@ let formatter = new Intl.NumberFormat('en-DE', {
    let toEuro = formatter.format(Number(document.getElementById("budget").value))
    console.log(toEuro);
 
+   
+   
 
 
 // This code I took and modified from 
 // https://www.ceos3c.com/javascript/store-user-input-in-a-variable-with-javascript/#:~:text=The%20JavaScript%20File,-The%20JavaScript%20part&text=To%20be%20able%20to%20store,input%20from%20the%20input%20form.
 
-function greetWithAlert(){
-    let momName = document.getElementById("username").value;
-    alert(`Hello ${momName}`)
-}
+        // ADD LATER FOR USERNAME
+// function greetWithAlert(){
+//     let momName = document.getElementById("username").value;
+//     alert(`Hello ${momName}`)
+// }
 
 let momBudget;
 function confirmBudget(){
     momBudget = document.getElementById("budget").value;
     alert(`Your budget of ${formatter.format(Number(momBudget))}  is confirmed`)
+}
+
+function addCost() {
+    alert(`Cost ${x}  added`)
 }
 
 
